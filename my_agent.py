52960d1fa1a645a3e0938b8015874f3a77e798a9
JonathanDou <jonathandou100@gmail.com>
2022-04-26 16:30:56 -0400

    commited

--- my_agent.py
+++ my_agent.py
@@ -49,6 +49,8 @@ class MyAgent(OneShotAgent):
 
         """
         # TODO
+        
+        self.secured = 0
 
     def propose(self, negotiator_id: str, state: MechanismState) -> "Outcome":
         """
@@ -62,6 +64,29 @@ class MyAgent(OneShotAgent):
             an outcome to offer.
         """
         # TODO
+        
+        return self.best_offer(negotiator_id)
+    
+    def best_offer(self, negotiator_id):
+        my_needs = self._needed(negotiator_id)
+        if my_needs <= 0:
+            return None
+        ami = self.get_nmi(negotiator_id)
+        if not ami:
+            return None
+        quantity_issue = ami.issues[QUANTITY]
+        unit_price_issue = ami.issues[UNIT_PRICE]
+        offer = [-1] * 3
+        offer[QUANTITY] = max(
+            min(my_needs, quantity_issue.max_value),
+            quantity_issue.min_value
+        )
+        offer[TIME] = self.awi.current_step
+        if self._is_selling(ami):
+            offer[UNIT_PRICE] = unit_price_issue.max_value
+        else:
+            offer[UNIT_PRICE] = unit_price_issue.min_value
+        return tuple(offer)
 
     def respond(
         self, negotiator_id: str, state: MechanismState, offer: "Outcome"
@@ -84,8 +109,23 @@ class MyAgent(OneShotAgent):
 
         """
         # TODO
-        return ResponseType.END_NEGOTIATION
+        my_needs = self._needed(negotiator_id)
+        if my_needs <= 0:
+            return ResponseType.END_NEGOTIATION
+        return (
+            ResponseType.ACCEPT_OFFER
+            if offer[QUANTITY] <= my_needs
+            else ResponseType.REJECT_OFFER
+        )
+    
+    def _needed(self, negotiator_id=None):
+        return self.awi.current_exogenous_input_quantity + \
+               self.awi.current_exogenous_output_quantity - \
+               self.secured
 
+    def _is_selling(self, ami):
+        return ami.annotation["product"] == self.awi.my_output_product
+    
     def on_negotiation_failure(
         self,
         partners: List[str],
@@ -119,6 +159,8 @@ class MyAgent(OneShotAgent):
                        about the negotiation that led to the `Contract` if any.
         """
         # TODO
+        
+        self.secured += contract.agreement["quantity"]
 
     def step(self):
         """
